9b6a233fa4af9862a97dffb359ed1dcbf15a19c7
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2022-11-14 00:07:27 -0600

    Update Pricing.tsx

--- components/Pricing.tsx
+++ components/Pricing.tsx
@@ -96,9 +96,9 @@ function Pricing() {
             initial={{ opacity: 0, y: 100 }}
             whileInView={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
+            key={feature.title}
         >
             <Card
-                key={feature.title}
                 shadow="md"
                 radius="md"
                 className={classes.card}
