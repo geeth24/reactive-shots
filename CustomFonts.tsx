4c2136a038e408fb2bbd0e7beba5ed01ad3580e4
Geeth S Gunnampalli <geeth.gunnampalli@gmail.com>
2022-12-06 09:58:03 -0600

    re add fonts

--- CustomFonts.tsx
+++ CustomFonts.tsx
@@ -4,30 +4,30 @@ export function CustomFonts() {
     return (
         <Global
             styles={[
-                // {
-                //     "@font-face": {
-                //         fontFamily: "Lato",
-                //         src: `url('/fonts/Lato-Bold.ttf') format('truetype')`,
-                //         fontWeight: 700,
-                //         fontStyle: "normal",
-                //     },
-                // },
-                // {
-                //     "@font-face": {
-                //         fontFamily: "Lato",
-                //         src: `url('/fonts/Lato-Regular.ttf') format('truetype')`,
-                //         fontWeight: 400,
-                //         fontStyle: "normal",
-                //     },
-                // },
-                // {
-                //     "@font-face": {
-                //         fontFamily: "Carter One",
-                //         src: `url('/fonts/CarterOne-Regular.ttf') format('truetype')`,
-                //         fontWeight: 400,
-                //         fontStyle: "normal",
-                //     },
-                // },
+                {
+                    "@font-face": {
+                        fontFamily: "Lato",
+                        src: `url('/fonts/Lato-Bold.ttf') format('truetype')`,
+                        fontWeight: 700,
+                        fontStyle: "normal",
+                    },
+                },
+                {
+                    "@font-face": {
+                        fontFamily: "Lato",
+                        src: `url('/fonts/Lato-Regular.ttf') format('truetype')`,
+                        fontWeight: 400,
+                        fontStyle: "normal",
+                    },
+                },
+                {
+                    "@font-face": {
+                        fontFamily: "Carter One",
+                        src: `url('/fonts/CarterOne-Regular.ttf') format('truetype')`,
+                        fontWeight: 400,
+                        fontStyle: "normal",
+                    },
+                },
                 //disable scrollbars
                 {
                     "::-webkit-scrollbar": {
