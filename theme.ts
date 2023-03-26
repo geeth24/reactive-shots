2c267cd4234401e9374ccc7bdb5e7a2cd86aafa4
Geeth Gunnampalli <geeth.gunnampalli@gmail.com>
2023-03-25 20:49:22 -0500

    fix

--- theme.ts
+++ theme.ts
@@ -4,7 +4,6 @@ const props = {
     "--my-white": "#fff",
     "--my-black": "#1a1a1a",
     "--my-blue": "#4285f4",
-    "--my-blue": "#db4437",
     "--my-yellow": "#f4b400",
     "--my-green": "#0f9d58",
 }
